34b0841809fd1e59a43a9f8ef586a69688f91f80
githupwzy213 <114745030+githupwzy213@users.noreply.github.com>
2022-10-02 17:28:19 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,42 @@
+#include"test.h"
+#include<string.h>
+int main()
+{
+	/*int count = 0;
+	int year = 0;
+	printf("1000-2000的闰年是：");
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (1 == is_leap_year(year))
+		{
+			printf("%d年 ", year);
+			count++;
+		}
+	}
+	printf("有%d个闰年\n", count);*/
+
+	//二分法查找
+	/*int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 0;
+	printf("请输入要查找的值\n");
+	scanf("%d", &k);
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	binary_search(arr,k, sz);*/
+
+	//每调用一次num+1
+	//int num = 0;
+	//Add(&num);
+	//printf("%d\n", num);//调用一次后num加一
+	//Add(&num);
+	//printf("%d\n", num);//调用两次num=2
+
+	//链式访问
+	//char arr[] = "abc";
+	//int len = strlen(arr);
+	//printf("%d\n", len);//打印字符窜长度
+	//printf("%d",strlen(arr));//链式访问
+	//printf("%d", 43);
+	//printf("%d", printf("%d", printf("%d", 43))); //返回1
+	
+	
+}
